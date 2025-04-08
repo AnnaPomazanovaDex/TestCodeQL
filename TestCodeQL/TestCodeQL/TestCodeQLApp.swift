f7c69c23e6b80dac845b303b20be0bc95f9ffd73
Anna Pomazanova <anna.pomazanova@dexcom.com>
2025-04-08 18:30:52 +0300

    Added project

---
+++ TestCodeQL/TestCodeQL/TestCodeQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCodeQLApp.swift
+//  TestCodeQL
+//
+//  Created by Anna Pomazanova on 08.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestCodeQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
